29118fb052359736044ac5809f79b387f44def3d
Daniel Peelen <dapeelen@gmail.com>
2025-05-03 17:37:02 -0400

    fixed images, added a few compliments

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,3 +1,36 @@
+import couple1 from '../img/IMG_6301.jpg';
+import couple2 from '../img/IMG_6290.jpg';
+import couple3 from '../img/IMG_6756.jpg';
+import couple4 from '../img/IMG_6837.JPG';
+import couple5 from '../img/IMG_6851.JPG';
+import couple6 from '../img/IMG_6854.JPG';
+import couple7 from '../img/IMG_6922.jpg';
+import couple8 from '../img/IMG_7044.JPG';
+import couple9 from '../img/IMG_7089.jpg';
+import couple10 from '../img/IMG_7129.jpg';
+import couple11 from '../img/IMG_7306.jpg';
+import couple12 from '../img/IMG_7834.jpg';
+import couple13 from '../img/IMG_7871.jpg';
+import couple14 from '../img/IMG_8013.jpg';
+import couple15 from '../img/IMG_8207.jpg';
+import couple16 from '../img/IMG_9081.jpg';
+import solo1 from '../img/FullSizeRender.jpg';
+import solo2 from '../img/IMG_6259.JPG';
+import solo3 from '../img/IMG_6642.jpg';
+import solo4 from '../img/IMG_7574.jpg';
+import solo5 from '../img/IMG_7579.jpg';
+import solo6 from '../img/IMG_7971.jpg';
+import solo7 from '../img/IMG_8198.jpg';
+import solo8 from '../img/IMG_8537.jpg';
+import funny1 from '../img/IMG_7482.PNG';
+import funny2 from '../img/IMG_7684.jpg';
+import funny3 from '../img/IMG_7976.jpg';
+import funny4 from '../img/IMG_8104.jpg';
+import funny5 from '../img/IMG_9128.jpg';
+import sleepy1 from '../img/IMG_7168.jpg';
+import sleepy2 from '../img/IMG_8028.jpg';
+import sleepy3 from '../img/IMG_8185.jpg';
+
 export default function Gallery() {
     return (
         <div class="gallery">
@@ -17,38 +50,38 @@ export default function Gallery() {
                         <h1>Cute Couple Pics</h1>
                         <p>These are pictures from parties or time we've spent with each exploring :)</p>
                         <div className="pics">
-                            <img src="../img/IMG_6301.jpg" alt="Couple Pic 1" />
-                            <img src="../img/IMG_6290.jpg" alt="Couple Pic 2" />
-                            <img src="../img/IMG_6756.jpg" alt="Couple Pic 3" />
-                            <img src="../img/IMG_6837.JPG" alt="Couple Pic 4" />
-                            <img src="../img/IMG_6851.JPG" alt="Couple Pic 5" />
-                            <img src="../img/IMG_6854.JPG" alt="Couple Pic 6" />
-                            <img src="../img/IMG_6922.jpg" alt="Couple Pic 7" />
-                            <img src="../img/IMG_7044.JPG" alt="Couple Pic 8" />
-                            <img src="../img/IMG_7089.jpg" alt="Couple Pic 9" />
-                            <img src="../img/IMG_7129.jpg" alt="Couple Pic 10" />
-                            <img src="../img/IMG_7306.jpg" alt="Couple Pic 11" />
-                            <img src="../img/IMG_7834.jpg" alt="Couple Pic 12" />
-                            <img src="../img/IMG_7871.jpg" alt="Couple Pic 13" />
-                            <img src="../img/IMG_8013.jpg" alt="Couple Pic 14" />
-                            <img src="../img/IMG_8207.jpg" alt="Couple Pic 15" />
-                            <img src="../img/IMG_9081.jpg" alt="Couple Pic 16" />
-                            <img src="../img/IMG_6301.jpg" alt="Couple Pic 1" />
-                            <img src="../img/IMG_6290.jpg" alt="Couple Pic 2" />
-                            <img src="../img/IMG_6756.jpg" alt="Couple Pic 3" />
-                            <img src="../img/IMG_6837.JPG" alt="Couple Pic 4" />
-                            <img src="../img/IMG_6851.JPG" alt="Couple Pic 5" />
-                            <img src="../img/IMG_6854.JPG" alt="Couple Pic 6" />
-                            <img src="../img/IMG_6922.jpg" alt="Couple Pic 7" />
-                            <img src="../img/IMG_7044.JPG" alt="Couple Pic 8" />
-                            <img src="../img/IMG_7089.jpg" alt="Couple Pic 9" />
-                            <img src="../img/IMG_7129.jpg" alt="Couple Pic 10" />
-                            <img src="../img/IMG_7306.jpg" alt="Couple Pic 11" />
-                            <img src="../img/IMG_7834.jpg" alt="Couple Pic 12" />
-                            <img src="../img/IMG_7871.jpg" alt="Couple Pic 13" />
-                            <img src="../img/IMG_8013.jpg" alt="Couple Pic 14" />
-                            <img src="../img/IMG_8207.jpg" alt="Couple Pic 15" />
-                            <img src="../img/IMG_9081.jpg" alt="Couple Pic 16" />
+                            <img src={couple1} alt="Couple Pic 1" />
+                            <img src={couple2} alt="Couple Pic 2" />
+                            <img src={couple3} alt="Couple Pic 3" />
+                            <img src={couple4} alt="Couple Pic 4" />
+                            <img src={couple5} alt="Couple Pic 5" />
+                            <img src={couple6} alt="Couple Pic 6" />
+                            <img src={couple7} alt="Couple Pic 7" />
+                            <img src={couple8} alt="Couple Pic 8" />
+                            <img src={couple9} alt="Couple Pic 9" />
+                            <img src={couple10} alt="Couple Pic 10" />
+                            <img src={couple11} alt="Couple Pic 11" />
+                            <img src={couple12} alt="Couple Pic 12" />
+                            <img src={couple13} alt="Couple Pic 13" />
+                            <img src={couple14} alt="Couple Pic 14" />
+                            <img src={couple15} alt="Couple Pic 15" />
+                            <img src={couple16} alt="Couple Pic 16" />
+                            <img src={couple1} alt="Couple Pic 1" />
+                            <img src={couple2} alt="Couple Pic 2" />
+                            <img src={couple3} alt="Couple Pic 3" />
+                            <img src={couple4} alt="Couple Pic 4" />
+                            <img src={couple5} alt="Couple Pic 5" />
+                            <img src={couple6} alt="Couple Pic 6" />
+                            <img src={couple7} alt="Couple Pic 7" />
+                            <img src={couple8} alt="Couple Pic 8" />
+                            <img src={couple9} alt="Couple Pic 9" />
+                            <img src={couple10} alt="Couple Pic 10" />
+                            <img src={couple11} alt="Couple Pic 11" />
+                            <img src={couple12} alt="Couple Pic 12" />
+                            <img src={couple13} alt="Couple Pic 13" />
+                            <img src={couple14} alt="Couple Pic 14" />
+                            <img src={couple15} alt="Couple Pic 15" />
+                            <img src={couple16} alt="Couple Pic 16" />
                         </div>
                     </div>
                 </section>
@@ -58,25 +91,22 @@ export default function Gallery() {
                         <h1>Solo Pics</h1>
                         <p>These are solo shots of my beautiful girlfriend, who I cherish getting to talk to every dayyyy</p>
                         <div className="pics">
-                            <img src="../img/FullSizeRender.jpg" alt="Couple Pic 1" />
-                            <img src="../img/IMG_6259.JPG" alt="Couple Pic 2" />
-                            <img src="../img/IMG_6642.jpg" alt="Couple Pic 3" />
-                            <img src="../img/IMG_7574.jpg" alt="Couple Pic 4" />
-                            <img src="../img/IMG_7579.jpg" alt="Couple Pic 5" />
-                            <img src="../img/IMG_7971.jpg" alt="Couple Pic 6" />
-                            <img src="../img/IMG_8198.jpg" alt="Couple Pic 7" />
-                            <img src="../img/IMG_8537.jpg" alt="Couple Pic 8" />
-                            <img src="../img/FullSizeRender.jpg" alt="Couple Pic 1" />
-                            <img src="../img/IMG_6259.JPG" alt="Couple Pic 2" />
-                            <img src="../img/IMG_6642.jpg" alt="Couple Pic 3" />
-                            <img src="../img/FullSizeRender.jpg" alt="Couple Pic 1" />
-                            <img src="../img/IMG_6259.JPG" alt="Couple Pic 2" />
-                            <img src="../img/IMG_6642.jpg" alt="Couple Pic 3" />
-                            <img src="../img/IMG_7574.jpg" alt="Couple Pic 4" />
-                            <img src="../img/IMG_7579.jpg" alt="Couple Pic 5" />
-                            <img src="../img/IMG_7971.jpg" alt="Couple Pic 6" />
-                            <img src="../img/IMG_8198.jpg" alt="Couple Pic 7" />
-                            <img src="../img/IMG_8537.jpg" alt="Couple Pic 8" />
+                            <img src={solo1} alt="Solo Pic 1" />
+                            <img src={solo2} alt="Solo Pic 2" />
+                            <img src={solo3} alt="Solo Pic 3" />
+                            <img src={solo4} alt="Solo Pic 4" />
+                            <img src={solo5} alt="Solo Pic 5" />
+                            <img src={solo6} alt="Solo Pic 6" />
+                            <img src={solo7} alt="Solo Pic 7" />
+                            <img src={solo8} alt="Solo Pic 8" />
+                            <img src={solo1} alt="Solo Pic 1" />
+                            <img src={solo2} alt="Solo Pic 2" />
+                            <img src={solo3} alt="Solo Pic 3" />
+                            <img src={solo4} alt="Solo Pic 4" />
+                            <img src={solo5} alt="Solo Pic 5" />
+                            <img src={solo6} alt="Solo Pic 6" />
+                            <img src={solo7} alt="Solo Pic 7" />
+                            <img src={solo8} alt="Solo Pic 8" />
                         </div>
                     </div>
                 </section>
@@ -86,16 +116,16 @@ export default function Gallery() {
                         <h1>Funny Pics</h1>
                         <p>These are just quirky lil photos of you pooks, that remind me of the silly funny side of you I love so much</p>
                         <div className="pics">
-                            <img src="../img/IMG_7482.PNG" alt="Couple Pic 1" />
-                            <img src="../img/IMG_7684.jpg" alt="Couple Pic 2" />
-                            <img src="../img/IMG_7976.jpg" alt="Couple Pic 3" />
-                            <img src="../img/IMG_8104.jpg" alt="Couple Pic 4" />
-                            <img src="../img/IMG_9128.jpg" alt="Couple Pic 5" />
-                            <img src="../img/IMG_7482.PNG" alt="Couple Pic 1" />
-                            <img src="../img/IMG_7684.jpg" alt="Couple Pic 2" />
-                            <img src="../img/IMG_7976.jpg" alt="Couple Pic 3" />
-                            <img src="../img/IMG_8104.jpg" alt="Couple Pic 4" />
-                            <img src="../img/IMG_9128.jpg" alt="Couple Pic 5" />
+                            <img src={funny1} alt="Funny Pic 1" />
+                            <img src={funny2} alt="Funny Pic 2" />
+                            <img src={funny3} alt="Funny Pic 3" />
+                            <img src={funny4} alt="Funny Pic 4" />
+                            <img src={funny5} alt="Funny Pic 5" />
+                            <img src={funny1} alt="Funny Pic 1" />
+                            <img src={funny2} alt="Funny Pic 2" />
+                            <img src={funny3} alt="Funny Pic 3" />
+                            <img src={funny4} alt="Funny Pic 4" />
+                            <img src={funny5} alt="Funny Pic 5" />
                             
                         </div>
                     </div>
@@ -106,15 +136,15 @@ export default function Gallery() {
                         <h1>Sleepy Pics</h1>
                         <p>And of course my favorite, the ones of you being my eepy little princess getting a good rest</p>
                         <div className="pics">
-                            <img src="../img/IMG_7168.jpg" alt="Couple Pic 1" />
-                            <img src="../img/IMG_8028.jpg" alt="Couple Pic 2" />
-                            <img src="../img/IMG_8185.jpg" alt="Couple Pic 3" />
-                            <img src="../img/IMG_7168.jpg" alt="Couple Pic 1" />
-                            <img src="../img/IMG_8028.jpg" alt="Couple Pic 2" />
-                            <img src="../img/IMG_8185.jpg" alt="Couple Pic 3" />
-                            <img src="../img/IMG_7168.jpg" alt="Couple Pic 1" />
-                            <img src="../img/IMG_8028.jpg" alt="Couple Pic 2" />
-                            <img src="../img/IMG_8185.jpg" alt="Couple Pic 3" />
+                            <img src={sleepy1} alt="Sleepy Pic 1" />
+                            <img src={sleepy2} alt="Sleepy Pic 2" />
+                            <img src={sleepy3} alt="Sleepy Pic 3" />
+                            <img src={sleepy1} alt="Sleepy Pic 1" />
+                            <img src={sleepy2} alt="Sleepy Pic 2" />
+                            <img src={sleepy3} alt="Sleepy Pic 3" />
+                            <img src={sleepy1} alt="Sleepy Pic 1" />
+                            <img src={sleepy2} alt="Sleepy Pic 2" />
+                            <img src={sleepy3} alt="Sleepy Pic 3" />
                         </div>
                     </div>
                 </section>
