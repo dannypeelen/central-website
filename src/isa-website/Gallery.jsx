1143da5c62e73ef669a8641b216b1b7d71446c3a
dannypeelen <dapeelen@gmail.com>
2026-01-22 21:46:50 -0500

    built in iloveisa endpoint

---
+++ src/isa-website/Gallery.jsx
@@ -0,0 +1,154 @@
+import couple1 from './img/IMG_6301.jpg';
+import couple2 from './img/IMG_6290.jpg';
+import couple3 from './img/IMG_6756.jpg';
+import couple4 from './img/IMG_6837.JPG';
+import couple5 from './img/IMG_6851.JPG';
+import couple6 from './img/IMG_6854.JPG';
+import couple7 from './img/IMG_6922.jpg';
+import couple8 from './img/IMG_7044.JPG';
+import couple9 from './img/IMG_7089.jpg';
+import couple10 from './img/IMG_7129.jpg';
+import couple11 from './img/IMG_7306.jpg';
+import couple12 from './img/IMG_7834.jpg';
+import couple13 from './img/IMG_7871.jpg';
+import couple14 from './img/IMG_8013.jpg';
+import couple15 from './img/IMG_8207.jpg';
+import couple16 from './img/IMG_9081.jpg';
+import solo1 from './img/FullSizeRender.jpg';
+import solo2 from './img/IMG_6259.JPG';
+import solo3 from './img/IMG_6642.jpg';
+import solo4 from './img/IMG_7574.jpg';
+import solo5 from './img/IMG_7579.jpg';
+import solo6 from './img/IMG_7971.jpg';
+import solo7 from './img/IMG_8198.jpg';
+import solo8 from './img/IMG_8537.jpg';
+import funny1 from './img/IMG_7482.PNG';
+import funny2 from './img/IMG_7684.jpg';
+import funny3 from './img/IMG_7976.jpg';
+import funny4 from './img/IMG_8104.jpg';
+import funny5 from './img/IMG_9128.jpg';
+import sleepy1 from './img/IMG_7168.jpg';
+import sleepy2 from './img/IMG_8028.jpg';
+import sleepy3 from './img/IMG_8185.jpg';
+
+export default function Gallery() {
+    return (
+        <div class="gallery">
+            <div className="gallery-intro">
+                <h1>Our Gallery &lt;3</h1>
+                <p>Welcome to Our lovely Gallery, where we can reminisce on all our great memories!</p>
+                <ul>
+                    <li><a href="#couple">cute couple pics</a></li>
+                    <li><a href="#solo">solo pics</a></li>
+                    <li><a href="#funny">funny cute pics</a></li>
+                    <li><a href="#sleepy">sleepy pics</a></li>
+                </ul>
+            </div>
+            <div className="gallery-images">
+                <section id="couple">
+                    <div className="couple-pics">
+                        <h1>Cute Couple Pics</h1>
+                        <p>These are pictures from parties or time we've spent with each exploring :)</p>
+                        <div className="pics">
+                            <img src={couple1} alt="Couple Pic 1" />
+                            <img src={couple2} alt="Couple Pic 2" />
+                            <img src={couple3} alt="Couple Pic 3" />
+                            <img src={couple4} alt="Couple Pic 4" />
+                            <img src={couple5} alt="Couple Pic 5" />
+                            <img src={couple6} alt="Couple Pic 6" />
+                            <img src={couple7} alt="Couple Pic 7" />
+                            <img src={couple8} alt="Couple Pic 8" />
+                            <img src={couple9} alt="Couple Pic 9" />
+                            <img src={couple10} alt="Couple Pic 10" />
+                            <img src={couple11} alt="Couple Pic 11" />
+                            <img src={couple12} alt="Couple Pic 12" />
+                            <img src={couple13} alt="Couple Pic 13" />
+                            <img src={couple14} alt="Couple Pic 14" />
+                            <img src={couple15} alt="Couple Pic 15" />
+                            <img src={couple16} alt="Couple Pic 16" />
+                            <img src={couple1} alt="Couple Pic 1" />
+                            <img src={couple2} alt="Couple Pic 2" />
+                            <img src={couple3} alt="Couple Pic 3" />
+                            <img src={couple4} alt="Couple Pic 4" />
+                            <img src={couple5} alt="Couple Pic 5" />
+                            <img src={couple6} alt="Couple Pic 6" />
+                            <img src={couple7} alt="Couple Pic 7" />
+                            <img src={couple8} alt="Couple Pic 8" />
+                            <img src={couple9} alt="Couple Pic 9" />
+                            <img src={couple10} alt="Couple Pic 10" />
+                            <img src={couple11} alt="Couple Pic 11" />
+                            <img src={couple12} alt="Couple Pic 12" />
+                            <img src={couple13} alt="Couple Pic 13" />
+                            <img src={couple14} alt="Couple Pic 14" />
+                            <img src={couple15} alt="Couple Pic 15" />
+                            <img src={couple16} alt="Couple Pic 16" />
+                        </div>
+                    </div>
+                </section>
+                <br/>
+                <section id="solo">
+                    <div className="solo-pics">
+                        <h1>Solo Pics</h1>
+                        <p>These are solo shots of my beautiful girlfriend, who I cherish getting to talk to every dayyyy</p>
+                        <div className="pics">
+                            <img src={solo1} alt="Solo Pic 1" />
+                            <img src={solo2} alt="Solo Pic 2" />
+                            <img src={solo3} alt="Solo Pic 3" />
+                            <img src={solo4} alt="Solo Pic 4" />
+                            <img src={solo5} alt="Solo Pic 5" />
+                            <img src={solo6} alt="Solo Pic 6" />
+                            <img src={solo7} alt="Solo Pic 7" />
+                            <img src={solo8} alt="Solo Pic 8" />
+                            <img src={solo1} alt="Solo Pic 1" />
+                            <img src={solo2} alt="Solo Pic 2" />
+                            <img src={solo3} alt="Solo Pic 3" />
+                            <img src={solo4} alt="Solo Pic 4" />
+                            <img src={solo5} alt="Solo Pic 5" />
+                            <img src={solo6} alt="Solo Pic 6" />
+                            <img src={solo7} alt="Solo Pic 7" />
+                            <img src={solo8} alt="Solo Pic 8" />
+                        </div>
+                    </div>
+                </section>
+                <br/>
+                <section id="funny">
+                    <div className="funny-pics">
+                        <h1>Funny Pics</h1>
+                        <p>These are just quirky lil photos of you pooks, that remind me of the silly funny side of you I love so much</p>
+                        <div className="pics">
+                            <img src={funny1} alt="Funny Pic 1" />
+                            <img src={funny2} alt="Funny Pic 2" />
+                            <img src={funny3} alt="Funny Pic 3" />
+                            <img src={funny4} alt="Funny Pic 4" />
+                            <img src={funny5} alt="Funny Pic 5" />
+                            <img src={funny1} alt="Funny Pic 1" />
+                            <img src={funny2} alt="Funny Pic 2" />
+                            <img src={funny3} alt="Funny Pic 3" />
+                            <img src={funny4} alt="Funny Pic 4" />
+                            <img src={funny5} alt="Funny Pic 5" />
+                            
+                        </div>
+                    </div>
+                </section>
+                <br/>
+                <section id="sleepy">
+                    <div className="sleepy-pics">
+                        <h1>Sleepy Pics</h1>
+                        <p>And of course my favorite, the ones of you being my eepy little princess getting a good rest</p>
+                        <div className="pics">
+                            <img src={sleepy1} alt="Sleepy Pic 1" />
+                            <img src={sleepy2} alt="Sleepy Pic 2" />
+                            <img src={sleepy3} alt="Sleepy Pic 3" />
+                            <img src={sleepy1} alt="Sleepy Pic 1" />
+                            <img src={sleepy2} alt="Sleepy Pic 2" />
+                            <img src={sleepy3} alt="Sleepy Pic 3" />
+                            <img src={sleepy1} alt="Sleepy Pic 1" />
+                            <img src={sleepy2} alt="Sleepy Pic 2" />
+                            <img src={sleepy3} alt="Sleepy Pic 3" />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
+}
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,107 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import fs from 'fs'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'isa-website-base-path',
+      transformIndexHtml(html, ctx) {
+        if (ctx.path === '/isa-website.html' || ctx.filename === 'isa-website.html') {
+          // Update script and link paths to use /isa-website/ base
+          return html
+            .replace(/src="\/src\//g, 'src="/isa-website/src/')
+            .replace(/href="\/src\//g, 'href="/isa-website/src/')
+            .replace(/href="\/isa-website\/img\//g, 'href="/isa-website/img/')
+        }
+        return html
+      },
+      writeBundle(options, bundle) {
+        // Move isa-website.html to isa-website/index.html after build
+        const isaHtmlPath = path.join(options.dir, 'isa-website.html')
+        const isaDir = path.join(options.dir, 'isa-website')
+        const isaAssetsDir = path.join(isaDir, 'assets')
+        const isaIndexPath = path.join(isaDir, 'index.html')
+        
+        if (fs.existsSync(isaHtmlPath)) {
+          // Ensure isa-website directory exists
+          if (!fs.existsSync(isaDir)) {
+            fs.mkdirSync(isaDir, { recursive: true })
+          }
+          if (!fs.existsSync(isaAssetsDir)) {
+            fs.mkdirSync(isaAssetsDir, { recursive: true })
+          }
+          
+          // Move isa-website specific assets from root assets to isa-website/assets
+          const assetsDir = path.join(options.dir, 'assets')
+          if (fs.existsSync(assetsDir)) {
+            const files = fs.readdirSync(assetsDir)
+            files.forEach(file => {
+              // Move isa-* files and heart-icon to isa-website/assets
+              if (file.startsWith('isa-') || file.startsWith('heart-icon')) {
+                const srcPath = path.join(assetsDir, file)
+                const destPath = path.join(isaAssetsDir, file)
+                if (fs.existsSync(srcPath)) {
+                  fs.renameSync(srcPath, destPath)
+                }
+              }
+            })
+          }
+          
+          // Read the HTML file and update paths
+          let html = fs.readFileSync(isaHtmlPath, 'utf-8')
+          // Update isa-website specific assets to use /isa-website/ base
+          // But keep shared chunks (like client.js) in root /assets/
+          html = html.replace(/(href|src)="\/assets\/(isa-[^"]+)"/g, '$1="/isa-website/assets/$2"')
+          html = html.replace(/(href|src)="\/assets\/(heart-icon[^"]+)"/g, '$1="/isa-website/assets/$2"')
+          // Fix shared chunks that were incorrectly moved - they should stay in /assets/
+          html = html.replace(/(href|src)="\/isa-website\/assets\/(client-[^"]+)"/g, '$1="/assets/$2"')
+          // Write to isa-website/index.html
+          fs.writeFileSync(isaIndexPath, html)
+          // Remove the original file
+          fs.unlinkSync(isaHtmlPath)
+        }
+      }
+    }
+  ],
   base:'/',
   root: './',
+  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.PNG', '**/*.png', '**/*.jpeg', '**/*.JPEG'],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        isa: resolve(__dirname, 'isa-website.html')
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          // Check if this is the isa entry point
+          if (chunkInfo.name === 'isa' || (chunkInfo.facadeModuleId && chunkInfo.facadeModuleId.includes('isa-website'))) {
+            return 'isa-website/assets/[name]-[hash].js'
+          }
+          return 'assets/[name]-[hash].js'
+        },
+        chunkFileNames: (chunkInfo) => {
+          // Check if chunk belongs to isa-website by checking module IDs
+          if (chunkInfo.moduleIds && chunkInfo.moduleIds.some(id => id && id.includes('isa-website'))) {
+            return 'isa-website/assets/[name]-[hash].js'
+          }
+          return 'assets/[name]-[hash].js'
+        },
+        assetFileNames: (assetInfo) => {
+          // Check if asset belongs to isa-website
+          const moduleIds = assetInfo.moduleIds || []
+          const name = assetInfo.name || ''
+          if (name.includes('isa-website') || moduleIds.some(id => id && id.includes('isa-website'))) {
+            return 'isa-website/assets/[name]-[hash][extname]'
+          }
+          return 'assets/[name]-[hash][extname]'
+        }
+      }
+    }
+  }
 })
